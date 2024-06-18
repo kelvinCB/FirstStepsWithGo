db3903242082a74c4c54bccd05fabe0080cb76e6
Kelvin Calcano <kelvinr02@hotmail.com>
2024-06-18 16:42:41 -0400

    Imported remote puppy package

--- 007-mod-code-depend-01/main.go
+++ 007-mod-code-depend-01/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"fmt"
 
-	art "github.com/kelvinCB/FirstStepsWithGo/007-sub-folder"
+	art "github.com/kelvinCB/FirstStepsWithGo/007-sub-folder" //Local package
+	"github.com/kelvinCB/puppy"                               // Remote package
 )
 
 func main() {
-	fmt.Println("hello,", Name)
-	fmt.Println("Hello,", art.Article)
+	fmt.Println("hello,", Name)          //Local variable into same local package
+	fmt.Println("Hello,", art.Article)   //Local varible into another local package
+	fmt.Println("Hello,", puppy.Bark())  //Remote metodo from another repo on Github
+	fmt.Println("Hello,", puppy.Barks()) //Remote metodo from another repo on Github
 }
+
+//Notes:
+
+/*
+To import Remote package, import package that you want
+and then go to your terminal,
+go to your project and write command:
+go get -a
+
+With that command your go.mod file will include remote package
+and you can use its methods and variables
+
+info learned in:
+Local: https://www.youtube.com/watch?v=Nv8J_Ruc280&t=132s
+Remote: https://www.youtube.com/watch?v=rHjlaHYd1F4
+
+Udemy Section 8 until Modular code, #1
+link: https://www.udemy.com/course/learn-how-to-code/learn/lecture/37652382#announcements
+*/
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kelvinCB/FirstStepsWithGo
 
 go 1.22.3
+
+require github.com/kelvinCB/puppy v0.0.0-20240618190427-20a1550e9362
