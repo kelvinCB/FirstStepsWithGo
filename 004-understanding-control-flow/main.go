51472309350a36d6e627187942397102df65fafb
Kelvin <kelvinr02@hotmail.com>
2024-06-26 01:33:51 -0400

    Understanding go logic sequence

---
+++ 004-understanding-control-flow/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//SEQUENCE
+	fmt.Println("First statement to run")
+	fmt.Println("Second statement to run")
+	x := 40 //Third Statement to run
+	y := 5  //Fourth Statement to run
+	fmt.Printf("x=%v\ny=%v", x, y)
+}
