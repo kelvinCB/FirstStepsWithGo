1b9b6051ff26cb232561475d86b82a3821b2dfa2
Kelvin <kelvinr02@hotmail.com>
2024-06-26 02:27:13 -0400

    learning if-else-elseif conditionals

---
+++ 003-conditionals/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Conditionals
+
+	//If statements
+
+	x := 40
+	y := 5
+
+	if x < 42 {
+		fmt.Println("Less than the meaning of life")
+	}
+
+	if y > 10 {
+		fmt.Println("Mi alma adora la gloria de papá")
+	} else {
+		fmt.Println("Esto pasó a otra dimensión")
+	}
+
+	if x >= 45 {
+		fmt.Println("Wow! que ciencia")
+	} else if x == 40 {
+		fmt.Println("Heheheheheh")
+	} else {
+		fmt.Println("Te fundí la masa cefálica")
+	}
+
+}
