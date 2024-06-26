cd83bc331d0a1c4cf630c1305cd8a36ee5724ea1
Kelvin <kelvinr02@hotmail.com>
2024-06-26 02:03:41 -0400

    Taking notes of all operators in Go

--- 004-understanding-control-flow/main.go
+++ 004-understanding-control-flow/main.go
@@ -14,4 +14,60 @@ func main() {
 	x := 40 //Third Statement to run
 	y := 5  //Fourth Statement to run
 	fmt.Printf("x=%v\ny=%v", x, y)
+
+	// Operators
+
+	//Comparison Operators
+
+	/*
+	   == equal
+	   != not equal
+	   < less
+	   <= less or equal
+	   > greater than
+	   >= greater or equal than
+
+	*/
+
+	//Arithmetic operators
+
+	/*
+		+    sum                    integers, floats, complex values, strings
+		-    difference             integers, floats, complex values
+		*    product                integers, floats, complex values
+		/    quotient               integers, floats, complex values
+		%    remainder              integers
+
+		&    bitwise AND            integers
+		|    bitwise OR             integers
+		^    bitwise XOR            integers
+		&^   bit clear (AND NOT)    integers
+
+		<<   left shift             integer << integer >= 0
+		>>   right shift            integer >> integer >= 0
+	*/
+
+	//Logical Operators
+
+	/*
+		&&    conditional AND    p && q  is  "if p then q else false"
+		||    conditional OR     p || q  is  "if p then true else q"
+		!     NOT                !p      is  "not p"
+	*/
+
+	//Address operators
+
+	/*
+		&x
+		&a[f(2)]
+		&Point{2, 3}
+		*p
+		*pf(x)
+
+		var x *int = nil
+		*x   // causes a run-time panic
+		&*x  // causes a run-time panic
+
+	*/
+
 }
